0914a4f56aed051078986146025369f188d0cf41
Lachlan Ridley <lachlan@sharesies.co.nz>
2024-10-11 20:54:37 +0100

    Fri 11 Oct 2024 20:54:37 BST

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,4 +0,0 @@
-oh-my-posh init pwsh | Invoke-Expression
-
-# aliases
-New-Alias -Name czm -Value chezmoi
---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+				set BASE16_SHELL "$HOME/.config/base16-shell/"
+				source "$BASE16_SHELL/profile_helper.fish"
+end
+
+# Created by `pipx` on 2024-07-10 07:33:07
+set PATH $PATH /Users/lachlan/.local/bin
+
+set --global --export PATH $HOMEBREW_PREFIX/opt/grep/libexec/gnubin $PATH
+
+zoxide init fish | source
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+abbr --add lg lazygit
