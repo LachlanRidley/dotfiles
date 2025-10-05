b856e21a40b577a9da73b600f02c2e1c3c8e8d2c
Lachlan Ridley <lachlan@sharesies.co.nz>
2025-10-05 15:20:03 +1100

    Remove apt

--- apt/.config/fish/functions/apt_install.fish
+++
@@ -1,9 +0,0 @@
-function apt_install
-    set package $argv[1]
-
-    set result (is_apt_package_installed $package)
-
-    if test $result = "false"
-        sudo apt install -y $package
-    end
-end
